9f4e762e0095491556c0a63bbcf8dec49a4d3519
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2023-01-24 14:24:19 -0300

    Remplazo para usar Deno server api

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-import { serve } from "https://deno.land/std@0.173.0/http/mod.ts";
+const server = Deno.listen({ port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
 
-const handler = (req: Request): Response => {
-    return new Response(null, { status: 200 });
-};
+for await (const conn of server) {
+  serveHttp(conn);
+}
 
-serve(handler,{port:8080});
+async function serveHttp(conn: Deno.Conn): Promise<void> {
+  const httpConn = Deno.serveHttp(conn);
+
+  for await (const requestEvent of httpConn) {
+    console.log(requestEvent);
+
+    if (requestEvent.request.url == "http://localhost:8080/users") {
+      requestEvent.respondWith(new Response("[{'a':'a'}]", { status: 200 }));
+    } else {
+      requestEvent.respondWith(new Response("Not founds", { status: 404 }));
+    }
+  }
+}
